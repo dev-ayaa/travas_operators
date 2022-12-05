98fee101c8528a0b3beab42adfadb58145d2ff93
dev-ayaa <ayaaakinleye@gmail.com>
2022-12-05 01:45:31 +0100

    db collection tours and tour guides

--- internal/query/collection.go
+++ internal/query/collection.go
@@ -3,6 +3,16 @@ package query
 import "go.mongodb.org/mongo-driver/mongo"
 
 func OperatorData(db *mongo.Client, collection string) *mongo.Collection {
-	var operator = db.Database("travas").Collection("operator")
-	return operator
+	var operators = db.Database("travas").Collection("operators")
+	return operators
+}
+
+func TourData(db *mongo.Client, collection string) *mongo.Collection {
+	var tours = db.Database("travas").Collection("tours")
+	return tours
+}
+
+func TourGuideData(db *mongo.Client, collection string) *mongo.Collection {
+	var guide = db.Database("travas").Collection("tour_guides")
+	return guide
 }
